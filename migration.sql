d58015bd3933f30a9e8ca032ef0e4d650b6edc2f
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2023-02-10 09:29:39 -0600

    added stuff to migration sql

--- migration.sql
+++ migration.sql
@@ -1,22 +1,36 @@
-USE adlister_db;
+USE
+adlister_db;
 
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE users
+(
+    id       long UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(240) NOT NULL,
-    email VARCHAR(240) NOT NULL,
+    email    VARCHAR(240) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    imgURL   VARCHAR(255) NOT NULL,
+    isAdmin  boolean      NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE ads (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id INT UNSIGNED NOT NULL,
-    title VARCHAR(240) NOT NULL,
-    description TEXT NOT NULL,
+CREATE TABLE ads
+(
+    id          long UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id     long UNSIGNED NOT NULL,
+    title       VARCHAR(240) NOT NULL,
+    description TEXT         NOT NULL,
+    imgURL      VARCHAR(255) NOT NULL,
+    date_created(DATETIME),
+    price( double),
     PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users (id)
         ON DELETE CASCADE
+);
+
+CREATE TABLE category
+(
+    category_id          long UNSIGNED NOT NULL AUTO_INCREMENT,
+    name       VARCHAR(240) NOT NULL,
 );
