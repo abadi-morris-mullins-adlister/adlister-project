6e4ad84cb9a55168465dd3d9d1a7c77b2dd36e6f
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2023-02-10 10:23:10 -0600

    added joint table

--- migration.sql
+++ migration.sql
@@ -8,23 +8,23 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users
 (
     id       bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-    username VARCHAR(240) NOT NULL,
-    email    VARCHAR(240) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    imgURL   VARCHAR(255) NOT NULL,
-    isAdmin  boolean      NOT NULL,
+    username VARCHAR(240)    NOT NULL,
+    email    VARCHAR(240)    NOT NULL,
+    password VARCHAR(255)    NOT NULL,
+    imgURL   VARCHAR(255)    NOT NULL,
+    isAdmin  boolean         NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE ads
 (
-    id          bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id     bigint UNSIGNED NOT NULL,
-    title       VARCHAR(240) NOT NULL,
-    description TEXT         NOT NULL,
-    imgURL      VARCHAR(255) NOT NULL,
+    id           bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id      bigint UNSIGNED NOT NULL,
+    title        VARCHAR(240)    NOT NULL,
+    description  TEXT            NOT NULL,
+    imgURL       VARCHAR(255)    NOT NULL,
     date_created DATETIME,
-    price double,
+    price        double,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users (id)
         ON DELETE CASCADE
@@ -32,7 +32,16 @@ CREATE TABLE ads
 
 CREATE TABLE category
 (
-    category_id          bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-    name       VARCHAR(240) NOT NULL,
-        PRIMARY KEY (category_id)
+    category_id bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(240)    NOT NULL,
+    PRIMARY KEY (category_id)
+);
+
+CREATE TABLE ad_categories
+(
+    ad_id       bigint UNSIGNED NOT NULL,
+    category_id bigint UNSIGNED NOT NULL,
+    PRIMARY KEY (ad_id, category_id),
+    FOREIGN KEY (ad_id) REFERENCES ads (id),
+    FOREIGN KEY (category_id) REFERENCES category (category_id)
 );
